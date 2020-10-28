b5a2de5be16a0e21a96c5657bdf50d88d05fe0ce
Gabriel <gabriel.scalese@hotmail.com>
2020-10-28 14:35:28 -0300

    Update

--- main.py
+++ main.py
@@ -1,77 +1,96 @@
-# 1 - Import library
 import pygame
-from pygame.locals import *
+import time
 
-gramado = pygame.image.load("gramado.png")
-pedra = pygame.image.load("pedra.png") ## altura = 48
-rio = pygame.image.load("rio.png") ## altura = 245
-rua = pygame.image.load("rua.png") ## altura = 250
-frog = pygame.image.load("frog.png")
 
-images = [gramado, pedra, rio, rua]
+class Player:
+
+    def __init__(self):
+        self.xPlayer = w / 2
+        self.yPlayer = 570
+
+    def move(self, pressed):
+
+        if pressed[pygame.K_UP]:
+            self.yPlayer -= 1
+
+            if self.yPlayer < -2:
+                self.yPlayer = 623
+
+        elif pressed[pygame.K_DOWN]:
+            self.yPlayer += 1
+
+            if self.yPlayer > 623:
+                self.yPlayer = 623
+
+        elif pressed[pygame.K_RIGHT]:
+            self.xPlayer += 1
+
+            if self.xPlayer > 614:
+                self.xPlayer = 614
+
+        elif pressed[pygame.K_LEFT]:
+            self.xPlayer -= 1
+
+            if self.xPlayer < -4:
+                self.xPlayer = -4
+                time.sleep(5000)
+
+        screen.blit(imgPlayer, (self.xPlayer, self.yPlayer))
+
+
+class Car:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.speed = 0.5
+
+    def move(self):
+        if self.x == 490:
+            self.x = -self.x + 390
+
+        self.x += self.speed
+        screen.blit(imgCar, (self.x, self.y))
+
+
+background = pygame.image.load("background.png")
+background_size = background.get_size()
+w, h = background_size
+screen = pygame.display.set_mode((w, h))
 
 done = False
-
-x = 300
-y = 580
-
-# 2 - Initialize the game
 pygame.init()
-width, height = 650, 650
-screen = pygame.display.set_mode((width, height))
-
-# 3 - Load images
-player = pygame.image.load("background.png")
 pygame.display.set_caption("Frogger game")
-
-
 pygame.display.update()
 
-# 4 - keep looping through
+## Player
+imgPlayer = pygame.image.load("player.png")
+player = Player()
+
+## Car
+imgCar = pygame.image.load("car.png")
+cars = []
+x = 0
+
+## Linha 1
+for i in range(0, 8):
+    x += 120
+    cars.append(Car(x, 493))
+
+## Linha 2
+for i in range(0, 8):
+    x += 100
+    cars.append(Car(x, 449))
+
 while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
 
-    pressed = pygame.key.get_pressed()
-    if pressed[pygame.QUIT]:
-        # if it is quit the game
-        pygame.quit()
-        exit(0)
-    if pressed[pygame.K_UP]:
-        y -= 2
-
-    if (y < -2):
-        y = 623
-
-    if pressed[pygame.K_DOWN]:
-        y += 2
-
-        if (y > 623):
-            y = 623
-
-    if pressed[pygame.K_RIGHT]:
-        x += 2
-
-    if x > 614:
-        x = 614
-
-    if pressed[pygame.K_LEFT]:
-        x -= 2
-
-        if (x < -4):
-            x = -4
-
-    screen.fill(0)
-    # 6 - draw the screen elements
-
-    print("x: " + str(x) + " y: " + str(y))
-    screen.blit(pedra, (0, 600))
-    screen.blit(rua, (0, 351))
-    screen.blit(pedra, (0, 300))
-    screen.blit(rio, (0, 60))
-    screen.blit(gramado, (0, 0))
-    screen.blit(frog, (x, y))
-    pygame.display.flip()
-    ##pygame.display.update()
-
+    key = pygame.key.get_pressed()
+    screen.blit(background, (0, 0))
+    player.move(key)
+    print(player.yPlayer)
+    for car in cars:
+        car.move()
+    pygame.display.update()
