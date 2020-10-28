865f3c5fdafe134dbb12f03e96322940a435be8c
Gabriel <gabriel.scalese@hotmail.com>
2020-10-28 14:59:33 -0300

    Update

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import Type
 import pygame
 import time
 
@@ -43,14 +44,23 @@ class Car:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.speed = 0.5
+        self.speed = 1
 
     def move(self):
         if self.x == 490:
-            self.x = -self.x + 390
+            self.x = -self.x + 470
 
         self.x += self.speed
         screen.blit(imgCar, (self.x, self.y))
+
+
+class Messages:
+
+    def show_lost(self):
+        font = pygame.font.SysFont(None, 25)
+        black = (0, 0, 0)
+        text = font.render("You lost", True, black)
+        screen.blit(text, (200, 300))
 
 
 background = pygame.image.load("background.png")
@@ -73,24 +83,43 @@ cars = []
 x = 0
 
 ## Linha 1
-for i in range(0, 8):
+for i in range(0, 16):
     x += 120
     cars.append(Car(x, 493))
 
+x = 0
+
 ## Linha 2
-for i in range(0, 8):
-    x += 100
+for i in range(0, 16):
+    x += 150
     cars.append(Car(x, 449))
 
+x = 0
+
+## Linha 3
+for i in range(0, 16):
+    x += 180
+    cars.append(Car(x, 358))
+
+x = 0
+
+## Linha 4
+for i in range(0, 16):
+    x += 140
+    cars.append(Car(x, 398))
+
 while not done:
+    screen.blit(background, (0, 0))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
 
     key = pygame.key.get_pressed()
-    screen.blit(background, (0, 0))
     player.move(key)
-    print(player.yPlayer)
     for car in cars:
         car.move()
+        if car.y == player.yPlayer:
+            Messages.show_lost(Type[Messages])
+
     pygame.display.update()
